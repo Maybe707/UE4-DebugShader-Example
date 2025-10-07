8db3c9167e571f4a8adbadb7b7fa998d092f559b
Scream9 <fellfrostqwt@gmail.com>
2025-10-07 15:10:31 +0900

    move to cpp file RenderSimplePass function

--- Plugins/DebugShader/Source/DebugShader/Public/FDebugShader.h
+++ Plugins/DebugShader/Source/DebugShader/Public/FDebugShader.h
@@ -51,38 +51,4 @@ public:
 /*     return RenderTarget; */
 /* } */
 
-void RenderSimplePass(FRHICommandListImmediate& RHICmdList, FTexture2DRHIRef RenderTarget)
-{
-    // 1. Получаем шейдеры
-    TShaderMapRef<FDebugShader> VertexShader(GetGlobalShaderMap(GMaxRHIFeatureLevel));
-    TShaderMapRef<FMyPS> PixelShader(GetGlobalShaderMap(GMaxRHIFeatureLevel));
-
-    // 2. Начинаем рендер пасс
-    FRHIRenderPassInfo RenderPassInfo(RenderTarget, ERenderTargetActions::DontLoad_Store);
-    RHICmdList.BeginRenderPass(RenderPassInfo, TEXT("SimpleRenderPass"));
-
-    // 3. Настраиваем вьюпорт
-	FIntPoint TargetSize(1920, 1080); // Или ваш размер
-    RHICmdList.SetViewport(0, 0, 0.0f, TargetSize.X, TargetSize.Y, 1.0f);
-
-    // 4. Настраиваем графический пайплайн
-    FGraphicsPipelineStateInitializer GraphicsPSOInit;
-    RHICmdList.ApplyCachedRenderTargets(GraphicsPSOInit);
-    
-    GraphicsPSOInit.DepthStencilState = TStaticDepthStencilState<false, CF_Always>::GetRHI();
-    GraphicsPSOInit.BlendState = TStaticBlendState<>::GetRHI();
-    GraphicsPSOInit.RasterizerState = TStaticRasterizerState<>::GetRHI();
-    GraphicsPSOInit.PrimitiveType = PT_TriangleList;
-    
-    GraphicsPSOInit.BoundShaderState.VertexDeclarationRHI = GEmptyVertexDeclaration.VertexDeclarationRHI;
-    GraphicsPSOInit.BoundShaderState.VertexShaderRHI = VertexShader.GetVertexShader();
-    GraphicsPSOInit.BoundShaderState.PixelShaderRHI = PixelShader.GetPixelShader();
-
-    SetGraphicsPipelineState(RHICmdList, GraphicsPSOInit);
-
-    // 5. Рисуем 3 вершины (полноэкранный треугольник)
-    RHICmdList.DrawPrimitive(0, 1, 3);
-
-    // 6. Заканчиваем рендер пасс
-    RHICmdList.EndRenderPass();
-}
+void RenderSimplePass(FRHICommandListImmediate& RHICmdList, FTexture2DRHIRef RenderTarget);
